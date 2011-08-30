23515541218dbeb97c9e38fc07e09181b81e6228
Martin Klepsch <martinklepsch@gmail.com>
2011-08-30 12:40:12 +0200

    some handy aliases and keychain setup

---
+++ custom/aliases.zsh
@@ -0,0 +1,4 @@
+alias r="ranger"
+alias vx="vim ~/.xmonad/xmonad.hs"
+alias alert="vlc ~/Music/Parov\ Stelar/Parov_Stelar-Coco/210-parov_stelar-monster_\(original_version\).mp3 &"
+
---
+++ custom/keys.zsh
@@ -0,0 +1,1 @@
+eval `keychain --eval pokerboni prgmr github_mklappstuhl cct_mklepsch`
